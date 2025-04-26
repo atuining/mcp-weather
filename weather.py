78a262e1bc03b85281107e61bba4312c4ffe6fd2
atuining <nagpalpranay@gmail.com>
2025-04-26 20:39:32 +0530

    change port

--- weather.py
+++ weather.py
@@ -121,4 +121,4 @@ if __name__ == "__main__":
 
     app = create_starlette_app(mcp_server, debug=True)
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
