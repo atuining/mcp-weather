ba95677e1be538a38233f46e668c92c7bcb19aec
atuining <nagpalpranay@gmail.com>
2025-04-27 01:39:29 +0530

    use requirements.txt and exit if version < 3.10

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "===== Verifying Python version ====="
-python3 -c 'import sys; version = sys.version_info; assert version >= (3,10), f"Python 3.10 or above is required, for {version}"'
+python3 -c 'import sys; version = sys.version_info; assert version >= (3,10), f"Python 3.10 or above is required, for {version}"' || { echo "Python version check failed"; exit 1; }
 
 echo "===== Creating virtual environment ====="
 python3 -m venv venv
@@ -15,6 +15,6 @@ echo "===== Upgrading pip ====="
 pip install --upgrade pip
 
 echo "===== Installing dependencies ====="
-pip install .
+pip install -r requirements.txt
 
 echo "===== Build Complete ====="
