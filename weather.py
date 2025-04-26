faacfc0061fec7a9fabcae84455a38faa1dced3f
wizpranay <pranay.nagpal@wizlearnr.com>
2025-04-26 19:45:39 +0530

    create mcp server with weather forecast for 3 days

---
+++ weather.py
@@ -0,0 +1,73 @@
+import os
+from typing import Any
+from datetime import datetime
+
+import httpx
+import uvicorn
+from mcp.server.fastmcp import FastMCP
+from starlette.applications import Starlette
+from starlette.routing import Mount
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mcp = FastMCP(
+    name="weather", description="MCP for weather forecast for 3 days", version="1.0.0"
+)
+
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+WEATHER_API_BASE = "https://api.weatherapi.com/v1"
+NWS_API_BASE = "https://api.weather.gov"
+USER_AGENT = "weather-app/1.0"
+
+
+async def make_weather_request(url: str) -> dict[str, Any] | None:
+    """Make a request to the NWS API with proper error handling"""
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url, timeout=30.0)
+            response.raise_for_status()
+            return response.json()
+        except Exception:
+            return None
+
+
+@mcp.tool()
+async def get_forecast(location: str) -> str:
+    """Get weather forecast for a location.
+
+    Args:
+        location: Location as longitude,latitude e.g. "48.8567,2.3508" or city name e.g. "Paris" or IP address
+    """
+
+    url = f"{WEATHER_API_BASE}/forecast.json?key={WEATHER_API_KEY}&q={location}&days=3&hour={datetime.now().hour}"
+    weather_data = await make_weather_request(url)
+
+    if not weather_data:
+        return "Unable to fetch forecast data for this locaiton"
+
+    forecasts = weather_data["forecast"]["forecastday"]
+    pretty = []
+    for forecast in forecasts:
+        val = f"""
+{forecast["hour"][0]["time"]}:
+Temperature: {forecast["day"]["avgtemp_c"]}°C
+Max Wind: {forecast["day"]["maxwind_kph"]} kph
+Chance of rain: {forecast["day"]["daily_will_it_rain"]}
+Chance of snow: {forecast["day"]["daily_will_it_snow"]}
+UV: {forecast["day"]["uv"]}
+"""
+        pretty.append(val)
+
+    return "\n--\n".join(pretty)
+
+
+app = Starlette(
+    routes=[
+        Mount("/", app=mcp.sse_app()),
+    ]
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
